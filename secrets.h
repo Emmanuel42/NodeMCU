dd1916d2f35d5af11c538b07662a3dcc7d801983
Emmanuel42 <45388179+Emmanuel42@users.noreply.github.com>
2019-01-18 15:21:24 +0100

    Add files via upload

--- secrets.h
+++ secrets.h
@@ -1,8 +1,8 @@
 // Use this file to store all of the private credentials 
 // and connection details
 
-#define SECRET_SSID "Livebox-a1be"		// replace MySSID with your WiFi network name
-#define SECRET_PASS "vfke9x7"	// replace MyPassword with your WiFi password
+#define SECRET_SSID "MySSID"		// replace MySSID with your WiFi network name
+#define SECRET_PASS "MyPassword"	// replace MyPassword with your WiFi password
 
 #define SECRET_CH_ID 659369			// replace 0000000 with your channel number
 #define SECRET_WRITE_APIKEY "AYSSWY3D2NVY2CEP"   // replace XYZ with your channel write API Key
